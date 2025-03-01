5d42823c47066118e64c4ab8c9d617eee4cc6636
Baasit Bolaji Ayomiposi <bolajibaasit14@gmail.com>
2025-03-01 11:06:59 +0100

    Create __init__.py

---
+++ VPN/__init__.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# List of allowed origins
+origins = [
+    "https://security.ghostroute.io",
+    "http://localhost:5173",
+]
+"""
+origins = ["*"]
+"""
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allow specific origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
+
+
+from VPN import models
+from database import engine
+models.Base.metadata.create_all(bind=engine)
+
+
+from VPN import (
+    routes, pydantic_models,
+    helper
+)
