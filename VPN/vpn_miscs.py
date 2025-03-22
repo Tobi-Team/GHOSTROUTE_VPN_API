1563c4bb7e92a67f333715d0869b02083ad9314a
Baasit Bolaji Ayomiposi <bolajibaasit14@gmail.com>
2025-03-22 15:04:53 +0100

    Create vpn_miscs.py

---
+++ VPN/vpn_miscs.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+SERVER_LIST = [
+    "64.20.51.206",
+    #
+    "68.183.118.41",
+    "157.245.85.1",
+    "167.99.220.220"
+]
+
+SERVER_DICT = {
+    "a": {
+        "type": "private",
+        "hosting": "residential",
+        "location": "Madrid (Espanol), Spain",
+        "price": "999.99"
+    },
+    "64.20.51.206": {
+        "type": "private",
+        "hosting": "residential",
+        "location": "New York, United States",
+        "price": "999.99"
+    },
+    "68.183.118.41": {
+        "type": "public",
+        "hosting": "droplet",
+        "location": "North Bergen, United States",
+        "price": "999.99"
+    },
+    "167.99.220.220": {
+        "type": "public",
+        "hosting": "droplet",
+        "location": "Amsterdam, Netherlands",
+        "price": "999.99"
+    },
+    "157.245.85.1": {
+        "type": "public",
+        "hosting": "droplet",
+        "location": "New York, United States",
+        "price": "999.99"
+    },
+}
