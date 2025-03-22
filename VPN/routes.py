574c6c8872a5899f815f792f273b7c18625dd533
Baasit Bolaji Ayomiposi <bolajibaasit14@gmail.com>
2025-03-22 14:58:23 +0100

    Update routes.py

--- VPN/routes.py
+++ VPN/routes.py
@@ -1,39 +1,12 @@
 from VPN import app
 from fastapi import status, Depends, HTTPException, Form
 
-"""
-from VPN.models import (
-)
-
-from QUIET.helper import (
-)
-
-from QUIET.pydantic_models import (
-)
-"""
-
-from database import SessionLocal
-from sqlalchemy.orm import Session
-from typing import Annotated
-
 # import jwt
 import json
-import time
 import os
-import re
 import httpx
 from httpx import Timeout
 from random import randint
-from passlib.hash import bcrypt_sha256
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-db_dependency = Annotated[Session, Depends(get_db)]
 
 
 #  [ INDEX/MISC ]
