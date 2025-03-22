3f34e11a1f84adf3adb79836e316446219b3dca6
Baasit Bolaji Ayomiposi <bolajibaasit14@gmail.com>
2025-03-22 15:12:09 +0100

    Update servers_routes.py

--- VPN/servers_routes.py
+++ VPN/servers_routes.py
@@ -1,1 +1,12 @@
+from VPN import app
+from fastapi import status, Depends, HTTPException, Form
 
+# get all servers
+# get all private servers + 100% anonymity + 98%+ speed + 99.99% privacy
+# get all public servers + 90%+ speed 
+
+
+@app.get("/server/get_all_servers", status_code=status.HTTP_200_OK)
+@app.get("/server/get_all_servers/", status_code=status.HTTP_200_OK)
+def get_all_servers():
+    # To return lists of all servers
